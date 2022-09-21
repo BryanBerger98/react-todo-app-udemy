c4f4f271346be423601dd966d6868314d10ce205
Bryan BERGER <contact@weberger.net>
2022-09-21 16:45:15 +0200

    initial commit

---
+++ src/Components/Tasks/TasksTable/TasksTable.js
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+import { TasksContext } from '../../../Contexts/TasksContext';
+import TaskRow from './TaskRow';
+import style from './TasksTable.module.css';
+
+const TasksTable = () => {
+
+	const { tasksData } = useContext(TasksContext);
+
+	return (
+		<div className={ style['tasks-table-container'] }>
+			<table className={ style['tasks-table'] }>
+				<thead>
+					<tr>
+						<th>Status</th>
+						<th>Title</th>
+						<th>Description</th>
+						<th>Created at</th>
+						<th>Time</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						tasksData && tasksData.tasks && tasksData.tasks.map((task, index) => (
+							<TaskRow key={ Date.parse(task.createdAt) - index } task={ task } index={ index } />
+						))
+					}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default TasksTable;
---
+++ src/Components/UI/PageTitle/PageTitle.js
@@ -0,0 +1,8 @@
+const PageTitle = ({ count, title }) => {
+	
+	return (
+		<h2>{ count } { title }</h2>
+	);
+};
+
+export default PageTitle;
